683a3bc7922d9c2556ff3c258631b6c77a0d6365
mercy17ch <60229557+mercy17ch@users.noreply.github.com>
2020-02-01 16:02:26 +0300

    Add files via upload

---
+++ areacircle.py
@@ -0,0 +1,5 @@
+""""program to calculate area of a circle"""
+pi=3.14
+r=int(input("enter the radious of the circle  "))
+area=pi*r*r
+print("the area of the circle is ", area)
---
+++ perimeterofarectangle.py
@@ -0,0 +1,9 @@
+"""area and perimeter of a rectangle"""
+print("enter the length and width of the rectangle")
+l=int(input("length"))
+w=int(input("width"))
+area=l*w
+perimeter=l+l+w+w
+print("the area=",area)
+print("the perimeter=",perimeter)
+
